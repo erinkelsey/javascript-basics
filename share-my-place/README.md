97108178e594e7ef98b884c9a380b3be14e3323b
erinkelsey <erinszilagyi@gmail.com>
2020-09-24 17:49:31 -0600

    implement share place

---
+++ share-my-place/README.md
@@ -0,0 +1,19 @@
+# Share My Place
+
+# Install
+
+    $ npm install
+
+# Run Development
+
+    $ npm run build:dev
+
+Navigate to: http://localhost:8080/
+
+# Run Production
+
+    $ npm run build:prod
+
+# Build
+
+    $ npm run build
---
+++ share-my-place/src/UI/Map.js
@@ -0,0 +1,21 @@
+export class Map {
+  constructor(coordinates) {
+    this.render(coordinates);
+  }
+
+  render(coordinates) {
+    if (!google) {
+      alert("Could not load maps library - please try again later!");
+    }
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: coordinates,
+      zoom: 16,
+    });
+
+    new google.maps.Marker({
+      position: coordinates,
+      map: map,
+    });
+  }
+}
